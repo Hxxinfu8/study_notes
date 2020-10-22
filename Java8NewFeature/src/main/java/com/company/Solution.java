78bb478ac4cd8bdc6764565400eef1c9c17238c2
hx <1156108448@qq.com>
2020-10-22 15:45:53 +0800

    leetcode刷题

--- Java8NewFeature/src/main/java/com/company/Solution.java
+++ Java8NewFeature/src/main/java/com/company/Solution.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 /**
  * Leetcode
+ *
  * @author hx
  */
 public class Solution {
     /**
      * 查找常用字符
+     *
      * @param A
      * @return
      */
@@ -46,7 +48,49 @@ public class Solution {
         return result;
     }
 
+    /**
+     * 搜索字符
+     * @param S
+     * @return
+     */
+    public static List<Integer> partitionLabels(String S) {
+        List<Integer> result = new ArrayList<>();
+        int[] a = new int[26];
+
+        for (int i = 0; i < S.length(); i++) {
+            a[S.charAt(i) - 'a'] = i;
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < S.length(); i++) {
+            end = Math.max(end, a[S.charAt(i) - 'a']);
+            if (i == end) {
+                result.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 计算器
+     * @param s
+     * @return
+     */
+    public int calculate(String s) {
+        int x = 1, y = 0;
+        for (int i = 0; i < s.length(); i ++) {
+            if (String.valueOf(s.charAt(i)).equals("A")) {
+                x = 2 * x + y;
+            }
+            if (String.valueOf(s.charAt(i)).equals("B")) {
+                y = 2 * y + x;
+            }
+        }
+        return x + y;
+    }
+
     public static void main(String[] args) {
-        commonChars(new String[]{"bella","label","roller"});
+        System.out.println(commonChars(new String[]{"bella", "label", "roller"}));
+        System.out.println(partitionLabels("abacsddfejjjh"));
     }
 }
